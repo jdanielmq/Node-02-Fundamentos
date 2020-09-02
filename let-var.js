4fa4a8532f748f21651f8deed04a4d70af379e24
Juan Daniel Muñoz Queupul <munoz.queupul.daniel.juan@gmail.com>
2020-09-02 16:40:07 -0400

    prueba con var

---
+++ let-var.js
@@ -0,0 +1,10 @@
+var nombre = 'Hola Juan daniel';
+if(true){
+   var nombre = ' francisco vera';
+}
+
+var nombre = ' francisco vera 1';
+var nombre = ' francisco vera 2';
+var nombre = ' francisco vera 3';
+var nombre = ' francisco vera 4';
+console.log(nombre);
