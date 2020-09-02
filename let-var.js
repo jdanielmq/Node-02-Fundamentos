9d0cd8074598b48b113ff9f9e7ba122ad07e9fd6
Juan Daniel Muñoz Queupul <munoz.queupul.daniel.juan@gmail.com>
2020-09-02 16:43:40 -0400

    prueba con let y mensaje de error si todos tiene let, la variable let solo se puede inicializar una vez en ejecucion.

--- let-var.js
+++ let-var.js
@@ -1,10 +1,12 @@
-var nombre = 'Hola Juan daniel';
+let nombre = 'Hola Juan daniel';
 if(true){
-   var nombre = ' francisco vera';
+    let nombre = ' francisco vera';
 }
 
-var nombre = ' francisco vera 1';
-var nombre = ' francisco vera 2';
-var nombre = ' francisco vera 3';
-var nombre = ' francisco vera 4';
+ nombre = ' francisco vera 1';
+ nombre = ' francisco vera 2';
+ nombre = ' francisco vera 3';
+ nombre = ' francisco vera 4';
+
+
 console.log(nombre);
